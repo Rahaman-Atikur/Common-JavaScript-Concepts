a209878e44e67af33709011c652cf87bf252a226
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-28 00:03:05 +0100

    Call Bsck function done

---
+++ callBackFunction.js
@@ -0,0 +1,17 @@
+function ghotok(patri,name){
+ patri();
+
+}
+const patri = function(messege){
+    console.log("Okay Ami paise");
+}
+const patri2 = function(messege){
+    console.log("Okay Patri2 paise");
+}
+
+const patri3 = function(messege){
+    console.log("Okay Patri3 paise");
+}
+ghotok(patri,"Hero alam");
+ghotok(patri2,"Hero alam");
+ghotok(patri3,"Hero alam");
